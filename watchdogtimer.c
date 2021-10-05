d03199e0d55f3be9369dd98d491463cb935ab42b
kailash neupane <34022713+hensal@users.noreply.github.com>
2021-10-05 13:53:38 +0900

    Add files via upload

---
+++ watchdogtimer.c
@@ -0,0 +1,83 @@
+/*******************************************************************************
+* DISCLAIMER
+* This software is supplied by Renesas Electronics Corporation and is only
+* intended for use with Renesas products. No other uses are authorized. This
+* software is owned by Renesas Electronics Corporation and is protected under
+* all applicable laws, including copyright laws.
+* THIS SOFTWARE IS PROVIDED "AS IS" AND RENESAS MAKES NO WARRANTIES REGARDING
+* THIS SOFTWARE, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING BUT NOT
+* LIMITED TO WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+* AND NON-INFRINGEMENT. ALL SUCH WARRANTIES ARE EXPRESSLY DISCLAIMED.
+* TO THE MAXIMUM EXTENT PERMITTED NOT PROHIBITED BY LAW, NEITHER RENESAS
+* ELECTRONICS CORPORATION NOR ANY OF ITS AFFILIATED COMPANIES SHALL BE LIABLE
+* FOR ANY DIRECT, INDIRECT, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES FOR
+* ANY REASON RELATED TO THIS SOFTWARE, EVEN IF RENESAS OR ITS AFFILIATES HAVE
+* BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+* Renesas reserves the right, without notice, to make changes to this software
+* and to discontinue the availability of this software. By using this software,
+* you agree to the additional terms and conditions found by accessing the
+* following link:
+* http://www.renesas.com/disclaimer
+*
+* Copyright (C) 2016 Renesas Electronics Corporation. All rights reserved.
+*******************************************************************************/
+
+/*******************************************************************************
+* File Name    : watchdogtimer.c
+* Version      : Applilet EZ PL for RL78
+* Device(s)    : R5F1056A,R5F1057A,R5F1058A
+* Tool-Chain   : CC-RL
+* Description  : This file implements main function.
+* Creation Date: 2016/09/14
+*******************************************************************************/
+
+/*******************************************************************************
+Includes <System Includes> , "Project Includes"
+*******************************************************************************/
+#include "sfr.h"
+#include "common.h"
+#include "config.h"
+#include "watchdogtimer.h"
+/*******************************************************************************
+Macro definitions
+*******************************************************************************/
+
+/*******************************************************************************
+Typedef definitions
+*******************************************************************************/
+
+/*******************************************************************************
+Imported global variables and functions (from other files)
+*******************************************************************************/
+
+/*******************************************************************************
+Exported global variables and functions (to be accessed by other files)
+*******************************************************************************/
+
+/*******************************************************************************
+Private variables and functions
+*******************************************************************************/
+
+/*******************************************************************************
+* Function Name: WDT_Init
+* Description  : This function initializes watchdogtimer module.
+* Arguments    : none
+* Return Value : none
+********************************************************************************/
+void WDT_Init( void )
+{
+    /*                                            */
+	/* WDT is set by the option byte in RL78/G11. */
+    /*                                            */
+}
+
+/*******************************************************************************
+* Function Name: WDT_Reset
+* Description  : This function watchdogtimer reset start.
+* Arguments    : none
+* Return Value : none
+********************************************************************************/
+void WDT_Reset( void )
+{
+	WDTE = 0x0AC;		/* Watchdog timer reset start */
+}
