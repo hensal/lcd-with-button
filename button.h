df2dcd63c45447b531bfb7f1fc1ad964a60a3349
kailash neupane <34022713+hensal@users.noreply.github.com>
2021-10-06 11:30:35 +0900

    Add files via upload

---
+++ button.h
@@ -0,0 +1,16 @@
+void IO_Init(void);
+void op_switich_in(void);
+unsigned char op_swin_1d(void);
+
+void op_switich_modef(void);
+void dispset_titl1(void);
+void op_switich_mdoef(void);
+
+extern unsigned char 	modef;
+extern unsigned char 	upf;
+extern unsigned char 	downf;
+extern unsigned char 	leftf;
+extern unsigned char 	rightf;
+extern unsigned char 	setf;
+
+
