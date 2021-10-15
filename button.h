2ceedcf6a6db44de089420ae52d432dcaf0b31b3
kailash neupane <34022713+hensal@users.noreply.github.com>
2021-10-15 16:19:02 +0900

    Add files via upload

--- button.h
+++ button.h
@@ -1,10 +1,15 @@
+#include <stdint.h>
 void IO_Init(void);
 void op_switich_in(void);
 unsigned char op_swin_1d(void);
 
-void op_switich_modef(void);
 void dispset_titl1(void);
-void op_switich_mdoef(void);
+void dispset_titl2(void);
+void dispset_titl3(void);
+void dispset_titl12345(void);
+void IntToStr(unsigned int intval);
+
+//int set_cursor_position (uint_fast8_t row, uint_fast8_t col);
 
 extern unsigned char 	modef;
 extern unsigned char 	upf;
@@ -12,5 +17,7 @@ extern unsigned char 	downf;
 extern unsigned char 	leftf;
 extern unsigned char 	rightf;
 extern unsigned char 	setf;
+
+void LCDInitialize(void);
 
 
