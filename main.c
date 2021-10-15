091e24823369491febe547db9b19bd10d2ebd2fd
kailash neupane <34022713+hensal@users.noreply.github.com>
2021-10-15 16:17:27 +0900

    Add files via upload

--- main.c
+++ main.c
@@ -16,8 +16,23 @@
 #include "watchdogtimer.h"
 #include "panel.h"
 #include "lcd_con.h"
+#include "button.h"
+#include "display.h"
+#include <stdint.h>
 
-void IO_Init(void);		
+void IO_Init(void);	
+void op_switich_in(void);		
+void op_switich_mdoef(void);
+void op_switich_modef(void);
+void op_switich_upf(void);
+void op_switich_downf(void);
+int op_switich_leftf(void);
+void op_switich_rightf(void);
+void op_switich_setf(void);
+void IntToStr(unsigned int intval);
+void op_switich_setff(void);
+
+int write_string(uint_fast8_t, uint_fast8_t,unsigned const char  *str);
 
 unsigned char 	modef 	= 0;		
 unsigned char 	upf 	= 0;
@@ -27,6 +42,9 @@ unsigned char 	rightf 	= 0;
 unsigned char 	setf 	= 0;
 
 unsigned char dpcnt[4];
+
+unsigned const char Line191[32] = "Hello, World";
+extern unsigned char Line22[32] = "9567   ";
 //************************************************************************
 //* Function Name: main
 //* Description  : This function implements main function.ÅiÉÅÉCÉìä÷êîÅj
@@ -37,8 +55,9 @@ void main( void );
 void main_loop( void );
 
 void main(void) {
-     /* Hardware Initiate */
-        hdwinit();
+     /* Hardware Initiate */     
+
+     hdwinit();
 
 	/* Panel Initiate */
 	panel_init0();
@@ -47,14 +66,17 @@ void main(void) {
 	lcd_init();
 	
 	/*LCD TITLE set */
-	dispset_titl();	
+	//dispset_titl();
+	
+	display1();
 	
 	/* Main Loop */
 	main_loop();	
 	
 	IO_Init();			//IOèâä˙âª
     	lcd_init();
-}
+	
+	}
 
 void main_loop(void)
     {    	
@@ -70,8 +92,24 @@ void main_loop(void)
 		dispset_count();
 		
 		op_switich_in();
-	       op_switich_mdoef();
-	
+		
+	        op_switich_mdoef();
+		
+		op_switich_upf();
+		
+		op_switich_downf();
+		
+		op_switich_leftf();
+		
+		op_switich_rightf();
+		
+		op_switich_setf();	
+		
+		
+		
+		//IntToStr(counterr);
+		
+		//op_switich_setff();	
 
 #if !defined PWM_USED
 		/* Processing at 10ms cycle */
@@ -81,3 +119,13 @@ void main_loop(void)
 #endif /* end of defined PWM_USED */
 	     }
     }
+ 
+/*void IntToStr(unsigned int intval) {
+        TITL111[0] = (tempp/1000) + 48;
+        TITL111[1] = (tempp/100)%10 + 48;
+        TITL111[2] = (tempp/10)%10 + 48;
+        TITL111[3] = (tempp/1)%10 + 48;
+       
+}
+*/
+
