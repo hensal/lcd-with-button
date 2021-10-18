600b8a89f913a3d91dd21d278afbc4ce8c868cfb
kailash neupane <34022713+hensal@users.noreply.github.com>
2021-10-18 09:26:21 +0900

    Add files via upload

--- lcd_con.h
+++ lcd_con.h
@@ -12,17 +12,14 @@ int lcd_cout(unsigned char ccod);
 int lcd_dout(unsigned char dcod);
 void delay_micro(unsigned int icnt);
 void delay_msec(unsigned int icnt);
-int write_string(uint_fast8_t row, uint_fast8_t col, unsigned const char  *str);
+
 void dispset_count(void);
-void show(unsigned char *s);
 
 void op_switich_upf(void);
 //**** •Ï”
 extern unsigned char dpcnt[4];
 
-int set_cursor_position (uint_fast8_t row, uint_fast8_t col);   
+int set_cursor_position (uint_fast8_t row, uint_fast8_t col); 
 
-
-//void write_string(uint_fast8_t line,uint_fast8_t col, unsigned char * str);
 
 void increment(void);
