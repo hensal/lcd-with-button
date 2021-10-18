3b6a72d5602d11bb592aa1f6dd43a2f819465f97
kailash neupane <34022713+hensal@users.noreply.github.com>
2021-10-18 09:25:44 +0900

    Add files via upload

--- button.h
+++ button.h
@@ -9,7 +9,8 @@ void dispset_titl3(void);
 void dispset_titl12345(void);
 void IntToStr(unsigned int intval);
 
-//int set_cursor_position (uint_fast8_t row, uint_fast8_t col);
+int set_cursor_position (uint_fast8_t row, uint_fast8_t col);
+int write_string(uint_fast8_t row, uint_fast8_t col, unsigned const char  *str);
 
 extern unsigned char 	modef;
 extern unsigned char 	upf;
